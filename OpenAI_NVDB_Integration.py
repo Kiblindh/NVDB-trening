d2918decf22c6588948dd353ad02c31c9eee02ff
Kiblindh <kimiverblindheimsvik@gmail.com>
2024-06-29 20:51:02 +0200

    Added another get-request to NVDB

--- OpenAI_NVDB_Integration.py
+++ OpenAI_NVDB_Integration.py
@@ -11,14 +11,14 @@ client.api_key = os.getenv("OPENAI_API_KEY")
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem about bugs in code."}
+    {"role": "system", "content": "You are an expert on information about norwegian roads"},
+    {"role": "user", "content": "Tell me the biggest road in Norway"}
   ]
 )
 
 
-def getNVDBInfo(url):
-  response = requests.get(url)
+def getNVDBInfo(id):
+  response = requests.get("https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/" + str(id) + "/statistikk") #example is id 107
   if response.status_code == 200:
     data = response.json()
     return str(data)
@@ -26,7 +26,19 @@ def getNVDBInfo(url):
   else:
     return "The request didn't work"
 
+
+def getAllNVDBFylker():
+  response = requests.get("https://nvdbapiles-v3.atlas.vegvesen.no/omrader/fylker")
+  if response.status_code == 200:
+    data = response.json()
+    return str(data)
+  
+  else:
+    return "The request didn't work"
+
+
     
 
-print(getNVDBInfo("https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekter/107/statistikk"))
+#print(getNVDBInfo(107))
+#print(getAllNVDBFylker())
 #print(completion.choices[0].message)    
